6f8f33aae7e7624ed8ac07ed29856d580da46ec0
Matthew Tribby <mrt28@duke.edu>
2018-04-09 14:35:25 -0400

    Basic shell classes, committing before workshop

---
+++ driver-optimization/src/main/java/driver/Crew.java
@@ -0,0 +1,17 @@
+package driver;
+
+import java.util.List;
+
+import com.graphhopper.jsprit.core.problem.Location;
+
+public class Crew {
+	private String id;
+	private Location origin;
+	private Location destination;
+	private int size;
+	private int dropOffPriority;
+	private List<String> stafferIds;
+	
+	//Create builder
+
+}
--- driver-optimization/src/main/java/driver/PwildDriver.java
+++ driver-optimization/src/main/java/driver/PwildDriver.java
@@ -1,12 +1,23 @@
 package driver;
 
 
-import com.graphhopper.jsprit.core.problem.driver.*;;
+import com.graphhopper.jsprit.core.problem.driver.*;
+import com.graphhopper.jsprit.core.util.Time;
 
 public class PwildDriver implements Driver{
+	
+	private String id;
+	private Time lastTimeDriven;
+	private String CrewID; 
+	
 
 	public String getId() {
-		return null;
+		return id;
+	}
+	
+	public int calculateTimeSinceLastDrive(Time newTime) {
+		//return newTime - lastTimeDriven;
+		return 0;
 	}
 
 }
